534d91d05f5abd89311b0cfcfde38b63846c335f
Danielle Demkiw <ddemkiw@gmail.com>
2015-03-30 19:26:11 +0100

    adding links

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'database_cleaner'
 
 group :test do
   gem 'rspec'
+  gem 'capybara'
 end
--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'data_mapper'
-require './app/model/link' 
+require './app/models/link' 
 
 env = ENV['RACK_ENV'] || 'development'
 
@@ -14,9 +14,17 @@ DataMapper.auto_upgrade!
 
 class Bookmark < Sinatra::Base
   
-  get '/' do
-    'Hello bookmark!'
-  end
+get '/' do
+  @links = Link.all
+  erb :index
+end
+
+post '/links' do
+  url = params["url"]
+  title = params["title"]
+  Link.create(:url => url, :title => title)
+  redirect to('/')
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
